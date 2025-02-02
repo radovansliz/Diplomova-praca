c4dd0eaf2d628508f7cc54e20534bb60f9e98148
Radovan Sliz <sliz.radovan@gmail.com>
2025-02-02 12:58:12 +0100

    Add random forest model and results

--- Classification Models/svm_v2.py
+++ Classification Models/svm_v2.py
@@ -32,9 +32,9 @@ X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_
 X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.4, random_state=42, stratify=y_temp)
 
 # Definícia modelu Support Vector Machine (SVM)
-print("🚀 Definujem SVM model s RBF jadrom...")
+print("🚀 Definujem SVM model s Linear jadrom...")
 svm_model = SVC(
-    kernel='rbf',          
+    kernel='linear',          
     C=1.0,                 
     gamma='scale',         
     random_state=42,       
